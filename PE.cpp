e89ca1a721ffa2c65182e2d941e8441c6582969b
Alexander Shi <ayshi@edu.uwaterloo.ca>
2017-10-30 11:15:22 -0400

    solved problem 18

--- PE.cpp
+++ PE.cpp
@@ -54,30 +54,49 @@ output_t problem19()
 // maximum vertical path sum for triangle
 output_t problem18()
 {
-	/*
-	75
-	95 64
-	17 47 82
-	18 35 87 10
-	20 04 82 47 65
-	19 01 23 75 03 34
-	88 02 77 73 07 63 67
-	99 65 04 28 06 16 70 92
-	41 41 26 56 83 40 80 70 33
-	41 48 72 33 47 32 37 16 94 29
-	53 71 44 65 25 43 91 52 97 51 14
-	70 11 33 28 77 73 17 78 39 68 17 57
-	91 71 52 38 17 14 91 43 58 50 27 29 48
-	63 66 04 68 89 53 67 30 73 16 69 87 40 31
-	04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-	*/
+	// using sublime, there are 120 elements (or use an gaussian sum for n = 15)
+	unsigned triangle[120] = {
+		75,
+		95, 64,
+		17, 47, 82,
+		18, 35, 87, 10,
+		20,  4, 82, 47, 65,
+		19,  1, 23, 75,  3, 34,
+		88,  2, 77, 73,  7, 63, 67,
+		99, 65,  4, 28,  6, 16, 70, 92,
+		41, 41, 26, 56, 83, 40, 80, 70, 33,
+		41, 48, 72, 33, 47, 32, 37, 16, 94, 29,
+		53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,
+		70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,
+		91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,
+		63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,
+		 4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23
+	};
 	// get next adjacent numbers
 		// store in an array and use offsets as function of row number?
+			// add (next row size) to get right child
+			// add (next row size - 1) to get left child
 		// store in a linked list structure?
 
-	// recursive function (getMax(left index, right index))
+	// BRUTE FORCE: recursive function (getMax(left index, right index))
 	// return max of max(left's left, left's right), max(right's left, right's right)
-	return -1;
+
+	// OPTIMIZED: solve for the triangle's maximum values from bottom up!
+	// resolve the n-1 row with the maximum path of each node row n-1 to row n
+
+	uint8_t cursor = 119; // index of last element of row 15
+	cursor -= 15; // index of last element of row 14
+	for (int row = 14; row > 0; --row) { // row index starts at 1, ends at and includes 15
+		cout << "\nrow " << row << "\n";
+		for (int i = 0; i < row; i ++) {
+			unsigned right_child = triangle[cursor + (row + 1)];
+			unsigned left_child  = triangle[cursor + (row + 1) - 1];
+			triangle[cursor] += left_child > right_child ? left_child : right_child;
+			cout << triangle[cursor] << " ";
+			--cursor;
+		}
+	} 
+	return triangle[0]; //1074
 }
 
 //letters used in listing numbers 1-1000 in words
